33e7cd3dfa982a8aab25f8ab199fd2a25cffc709
Evan Schoffstall <157645696+evanschoffstall@users.noreply.github.com>
2024-04-10 14:59:47 -0400

    chronark.com -> evanschoffstall.me

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,18 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "evanschoffstall.me",
+    template: "%s | evanschoffstall.me",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "evanschoffstall.me",
+    description: "Co-founder of unkey.dev and founder of planetfall.io",
+    url: "https://evanschoffstall.me",
+    siteName: "evanschoffstall.me",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://evanschoffstall.me/og.png",
         width: 1920,
         height: 1080,
       },
@@ -66,8 +65,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
