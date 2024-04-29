bd7b8ad2a42d99ab87490bf84a543fad89abd327
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-04-29 14:24:53 -0400

    Bugfix: Project/Contributions headers float on md

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -64,7 +64,7 @@ export default async function ProjectsPage() {
     <div className="relative pb-16">
       <Navigation />
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-        <div className="max-w-2xl mx-auto lg:mx-0">
+        <div className=""> {/* Scrubed so md doesn't float / 'unglue' TODO: add margin to md size*/}
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
             Projects
           </h2>
@@ -159,7 +159,7 @@ export default async function ProjectsPage() {
       <br></br>
       {/* Contributions*/}
       <div className="px-6 pt-10 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-12 md:pt-12 lg:pt-12">
-        <div className="max-w-2xl mx-auto lg:mx-0">
+        <div className=""> {/* Scrubed so md doesn't float / 'unglue' TODO: add margin to md size*/}
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-3xl">
             Contributions
           </h2>
