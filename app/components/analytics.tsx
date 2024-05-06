11ed38b33918529fc46766e8d80e71adcc011e2e
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-05-06 19:10:23 -0400

    with hardcode

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
 
 export function Analytics() {
-	const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+	const GTM_ID = "GTM-W9FBFT4R"
 
 	useEffect(() => {
 		if (typeof window !== 'undefined' && GTM_ID) {
