33be63e808b7cb79d459917961d72dd56f4989c6
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-05-11 19:21:19 -0400

    Custom gtag implementation with Cloudfare compatibility

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,15 +1,16 @@
 "use client";
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
 
 export function Analytics() {
-
-	useEffect(() => {
-		if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_GTM_ID) {
-			const TagManager = require('react-gtm-module');
-			TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID });
-		}
-	}, []);
-
-	return null;
+	return (
+		<script
+			dangerouslySetInnerHTML={{
+				__html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
+    `,
+			}}
+		/>
+	)
 }
