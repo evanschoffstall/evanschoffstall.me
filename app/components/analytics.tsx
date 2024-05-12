c24d09d108b6e44227969692911858ac48ecd628
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-05-12 10:49:29 -0400

    deprectate embedded Analytics in favor of Zaraz

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,19 +1,5 @@
 "use client";
 
 export function Analytics() {
-	return (
-		<>
-			<script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}></script>
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
-    `,
-				}}
-			/>
-		</>
-	)
+	return null
 }
