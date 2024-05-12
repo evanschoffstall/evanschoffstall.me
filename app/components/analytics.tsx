3a00086eb5bcdfaaeb8ac57ab290e6feeb69418d
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-05-12 10:28:33 -0400

    Add missing GTM script

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -2,15 +2,18 @@
 
 export function Analytics() {
 	return (
-		<script
-			dangerouslySetInnerHTML={{
-				__html: `
+		<>
+			<script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}></script>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
     `,
-			}}
-		/>
+				}}
+			/>
+		</>
 	)
 }
